71cbe8ae926447ff87aeacb3e03d03321e3d09d9
toof_jp <toof_jp@toof-jps-MacBook-Pro.local>
2025-09-01 10:51:03 +0900

    feat: update packages

--- brew.sh
+++ brew.sh
@@ -14,11 +14,11 @@ brew install tmux
 brew install mise
 brew install awscli
 brew install gnupg
+brew install starship
 brew install --cask iterm2
 brew install --cask brave-browser
 brew install --cask claude
 brew install --cask visual-studio-code
-brew install --cask cursor
 brew install --cask firefox
 brew install --cask alt-tab
 brew install --cask claude-code
